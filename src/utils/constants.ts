d23098e78fdb90d5e060ca1ff95f3db933ec5168
peter-popluhar <popluhar.peter@gmail.com>
2022-07-23 19:52:43 +0200

    term dynamically

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const DATE_FORMAT = "DD/MM/YYYY";
 export const API_URL_LOCATIONS =
-  "https://api.skypicker.com/locations?term=PRG&location_types=airport";
+  "https://api.skypicker.com/locations?location_types=airport";
 export const API_URL_FLIGHTS = `https://api.skypicker.com/flights?v=3&partner=skypicker&locale=en`;
